617ac173d39292732fdaca0e71e7c2896d349a49
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2025-05-10 15:13:36 -0300

    doc: adding explanations for the code logic

--- 2nd-assignment/1365-procurando-assentos/solution.cpp
+++ 2nd-assignment/1365-procurando-assentos/solution.cpp
@@ -10,6 +10,7 @@ int main() {
             for (int col = 0; col < columns; col++) {
                 char seat;
                 std::cin >> seat;
+                // Each position (i, j) represents the amount of available seats in the column j until row i-1.
                 available_seats[row + 1][col] = available_seats[row][col] + (seat == '.' ? 1 : 0);
             }
         }
@@ -20,6 +21,7 @@ int main() {
             for (int row2 = row1; row2 < rows; row2++) {
                 std::vector<int> col_free_seats(columns);
                 for (int col = 0; col < columns; col++) {
+                    // The number of available seats in column j, between row1 and row2
                     col_free_seats[col] = available_seats[row2 + 1][col] - available_seats[row1][col];
                 }
 
@@ -27,11 +29,17 @@ int main() {
                 for (int col = 0; col < columns; col++) {
                     total_free_seats += col_free_seats[col];
 
+                    // At some point, the total free seats between row1 and row2, counting up to column col,
+                    // may be greater than the number of friends.
+                    // If that is the case, free seats to the left of col can be removed from the total sum,
+                    // trying to minimize the area.
                     while (cols_left <= col && total_free_seats - col_free_seats[cols_left] >= friends) {
                         total_free_seats -= col_free_seats[cols_left];
                         cols_left++;
                     }
 
+                    // If the total free seats is greater than or equal to the number of friends,
+                    // we can calculate the area and check if it is the minimum area found so far.
                     if (total_free_seats >= friends) {
                         int height = row2 - row1 + 1;
                         int width = col - cols_left + 1;
